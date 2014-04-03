927a080d49313daa1288461bbb7a482bd9ad9433
David Lechner <david@lechnology.com>
2014-04-03 00:26:52 -0500

    add ability to hide updated date on pages

--- functions.php
+++ functions.php
@@ -25,7 +25,48 @@ add_shortcode('image','image_shortcode');
 
 
 function page_modified_date($args) {
-	if ( is_page() )
+	if ( is_page() && !get_post_meta( get_the_ID(), 'hide_page_modified_date', true ) )
 		echo '<span class="modified-date">Updated: ' . get_the_modified_date() . '</span>';
 }
 add_action('responsive_entry_top', 'page_modified_date');
+
+function render_hide_page_modified_date_meta_box( $page )
+{
+	wp_nonce_field( 'render_hide_page_modified_date_meta_box', 'render_hide_page_modified_date_meta_box_nonce' );
+	$value = get_post_meta( $page->ID, 'hide_page_modified_date', true );
+
+	echo '<input type="checkbox" id="hide_page_modified_date" name="hide_page_modified_date" ' . ( $value ? 'checked' : '' ) . ' />';
+	echo '<label for="hide_page_modified_date">Hide.</label>';
+}
+
+function hide_page_modified_date_meta_box( $post ) {
+	add_meta_box(
+		'hide-page-modified-date-meta-box',
+		__( 'Page Updated Date' ),
+		'render_hide_page_modified_date_meta_box',
+		'page',
+		'normal',
+		'default'
+	);
+}
+add_action( 'add_meta_boxes_page', 'hide_page_modified_date_meta_box' );
+
+function hide_page_modified_date_save_postdata ( $post_id )
+{
+	if ( ! isset( $_POST['render_hide_page_modified_date_meta_box_nonce'] ) )
+		return $post_id;
+
+	$nonce = $_POST['render_hide_page_modified_date_meta_box_nonce'];
+
+	if ( ! wp_verify_nonce( $nonce, 'render_hide_page_modified_date_meta_box' ) )
+		return $post_id;
+
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+		return $post_id;
+
+	if ( ! current_user_can( 'edit_page', $post_id ) )
+		return $post_id;
+
+	update_post_meta( $post_id, 'hide_page_modified_date', isset( $_POST['hide_page_modified_date'] ) );
+}
+add_action( 'save_post', 'hide_page_modified_date_save_postdata' );
